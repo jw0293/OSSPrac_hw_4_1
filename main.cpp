cb63ffc4c122f5e0203e02983ea74eaa82cf5138
jw0293 <k38836919@gmail.com>
2022-09-30 17:50:15 +0900

    add cpp file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define Mod 1000000007
+#define endl '\n'
+
+using namespace std;
+
+int main(){
+    fastio;
+
+    string name, id, major, school;
+    
+    cout << "이름을 입력하세요 : ";
+    cin >> name;
+
+    cout << "학번을 입력하세요 : ";
+    cin >> id;
+    
+    cout << endl << endl << "< 출력 >  " << endl;
+    cout << "이름 : " << name << endl;
+    cout << "힉번 : " << id << endl;
+    
+    return 0;
+}
